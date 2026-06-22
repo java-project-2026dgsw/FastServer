572872e897ada566d0849109fad6830fb593360a
최수아 <sua9097@gmail.com>
2026-06-22 11:38:46 +0900

    initial commit

---
+++ src/main/java/com/dgsw/fastserver/FastServerApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.fastserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgsw/fastserver/FastServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.fastserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
